432106c150e1057e0d8cfe5979bfe060a6c3f50b
guillaumew <geeyom12@gmail.com>
2016-09-17 16:02:18 +0200

    More Texts

--- public/scripts/elie.js
+++ public/scripts/elie.js
@@ -11,6 +11,7 @@ if (isInArea)
     document.getElementById('text').innerHTML = "<p>Tu trouves facilement le garage dont t'avais parlé Romain. Tu ouvres le garage... qui est presque totalement vide !</p><p>Tu vois un petit coffre dans un coin. C'est sûrement celui dont parlait Elie dans sa lettre. A coté de ce coffre, des morceaux de feuille déchirée.</p>"
     top.addObject(1);
     top.addObject(3);
+    top.openPlace(2);
   } else {
     document.getElementById('text').innerHTML = "<p>Tu te trouves au pied de l'immeuble de ce fameux Elie qui cherchait le trésor de Jules Verne. Malheureusement, la porte est fermée. Tu essaies de sonner chez les voisins, mais personne ne te répond.</p><p>Il vaut peut-être mieux rendre visite à son ami Romain.</p>";
   }
