023c14e770492b794c05f782534fa359d76afb02
guillaumew <geeyom12@gmail.com>
2016-09-17 12:21:29 +0200

    more js more obj

--- public/scripts/oye.js
+++ public/scripts/oye.js
@@ -7,6 +7,15 @@ var places = [
 	"is_findable" : true,
 	"was_found" : false,
 	"url" : "/elie"
+},
+{
+	"name" : "Appartement Romain",
+	"adresse" : "5, rue Voltaire, 44 000 Nantes",
+	"latitude" : 47.2124513,
+	"longitude" : -1.5640725000000657,
+	"is_findable" : true,
+	"was_found" : false,
+	"url" : "/romain"	
 }];
 
 var objets = [
@@ -14,15 +23,35 @@ var objets = [
 	"name" : "Lettre de Elie à Romain",
 	"url" : "/lettreElieRomain",
 	"thumb" : "/img/lettreElieRomain_thumb.jpg",
-	"was_found" : true
-}];
+	"was_found" : 0,
+	"number_to_find" : 1
+},
+{
+	"name" : "Lettre du proriétaire à Verne",
+	"url" : "/lettreProprioVerne",
+	"thumb" : "/img/lettreProprioVerne_thumb.jpg",
+	"was_found" : 0,
+	"number_to_find" : 3
+},
+{
+	"name" : "Clé du garage d'Elie",
+	"url" : "/clegarage",
+	"thumb" : "/img/clegarage_thumb.jpg",
+	"was_found" : 0,
+	"number_to_find" : 1
+}
+];
 
-function addObject(obj) {
-	var a_el = document.createElement("a");
-	a_el.href = obj.url;
-	a_el.setAttribute("target", "content");
-	var img_el = document.createElement("img");
-	img_el.src=obj.thumb;
-	a_el.appendChild(img_el);
-	document.getElementById("oye-object").appendChild(a_el);
+function addObject(index) {
+	obj = objets[index];
+	if(obj.was_found === 0){
+		objets[index].was_found++;
+		var a_el = document.createElement("a");
+		a_el.href = obj.url;
+		a_el.setAttribute("target", "content");
+		var img_el = document.createElement("img");
+		img_el.src=obj.thumb;
+		a_el.appendChild(img_el);
+		document.getElementById("oye-object").appendChild(a_el);
+	}
 }
