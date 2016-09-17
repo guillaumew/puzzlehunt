264c476c4e3ccf8ab2e6e5f10ec1d748c6efe2f9
guillaumew <geeyom12@gmail.com>
2016-09-17 14:59:47 +0200

    Texts in Elies

--- public/scripts/oye.js
+++ public/scripts/oye.js
@@ -109,6 +109,20 @@ var objets = [
 }
 ];
 
+// Fakeing position using URL params
+var params = {};
+
+if (location.search) {
+    var parts = location.search.substring(1).split('&');
+
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
+
+
 function addObject(index) {
 	obj = objets[index];
 	if(obj.was_found === 0){
@@ -131,8 +145,13 @@ function isPlaceFound(){
 	function displayPosition(position){
 		for(var i =0;i<places.length;i++){
 			if(places[i].is_findable){
-				var lat_place1 = position.coords.latitude;
-				var long_place1 = position.coords.longitude;
+				if(params.lat && params.long){
+					var lat_place1 = params.lat;
+					var long_place1 = params.long;
+				}else{
+					var lat_place1 = position.coords.latitude;
+					var long_place1 = position.coords.longitude;
+				}
 				var lat_place2 = places[i].latitude;
 				var long_place2 = places[i].longitude;
 				var distance =  Math.acos(Math.sin(lat_place1 * Math.PI / 180)*Math.sin((Math.PI / 180 * lat_place2))+Math.cos((Math.PI / 180 * lat_place1))*Math.cos((Math.PI / 180 * lat_place2))*Math.cos(Math.PI / 180 * (long_place1-long_place2)))*6371 ;
