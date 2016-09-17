a0a8a1bef341645413fc5536f6fed814480742e0
guillaumew <geeyom12@gmail.com>
2016-09-17 11:42:02 +0200

    begin script

---
+++ public/scripts/oye.js
@@ -0,0 +1,11 @@
+var places =[
+{
+	"name" : "Appartement Elie",
+	"adresse" : "4, rue Bâtonnier Guinaudeau, 44 000 Nantes",
+	"latitude" : 47.2102543,
+	"longitude" : -1.566128899999967,
+	"is_findable" : true,
+	"was_found" : false
+}];
+
+var objets = []
