5823a03866b6ad9caf56b434b7b0d9869d4aefa7
guillaumew <geeyom12@gmail.com>
2016-09-17 09:34:52 +0200

    new node server

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 var http = require('http');
-var fs = require('fs');
+var express = require('express');
 var geolocation = require('geolocation');
+require("twig");
 
+var app = express();
+var server =  http.Server(app);
 
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('./index.twig', {});
 });
 
-// Chargement de socket.io
-var io = require('socket.io').listen(server);
 
-// Quand un client se connecte, on le note dans la console
-
-io.sockets.on('connection', function (socket) {
-
-	socket.on('message', function (message) {
-		socket.broadcast.emit('message', message);
-
-	});	
-});
 
 server.listen(process.env.PORT || 5000);
